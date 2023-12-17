fd17a6a8fc8bcc9e4bcc2450065d412bac7cd4d4
donny_444 <104888835+donny444@users.noreply.github.com>
2023-12-17 21:29:42 +0700

    Add files via upload

--- pointer_practice.c
+++ pointer_practice.c
@@ -5,6 +5,19 @@ void main()
 	int* ptr = &myNum;	// A pointer variable, with the name ptr, that stores the address of myNum
 	printf("%p\n",&myNum);	// Output the memory address of myNum (444)
 	printf("%p\n",ptr);	// Output the memory address of myNum with the pointer
-	printf("%d",*ptr);	// Output the value of myNum with the pointer (444)
+	printf("%d\n",*ptr);	// Output the value of myNum with the pointer (444)
+	
+	int myNums[4] = {25, 50, 75, 100};
+	int i;
+	for(i = 0; i < 4; i++)
+	{
+		printf("%lu\n", sizeof(myNums[i]));
+		printf("%p\n", &myNums[i]);
+	}
+	printf("%p\n", myNums);
+	printf("%p\n", &myNums[0]);
+	printf("%d ", *myNums);
+	printf("%d ", *(myNums + 1));
+	printf("%d ", *(myNums + 2));
 	getch();
 }
